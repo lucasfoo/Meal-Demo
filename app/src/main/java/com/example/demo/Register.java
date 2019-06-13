84d754f457800db3bada317640d06613be54b0ce
shaopeng <sleepingpandaaa@gmail.com>
2019-06-13 11:13:28 +0800

    add in sign up and linked Rdetail page up(restaurant menu)

---
+++ app/src/main/java/com/example/demo/Register.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        button = (Button) findViewById(R.id.back_to_login_page);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
