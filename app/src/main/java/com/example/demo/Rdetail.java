0323457cc14514e6d0cf7bf58d60ce6261b88f98
shaopeng <sleepingpandaaa@gmail.com>
2019-06-30 16:09:55 +0800

    revert back for Rdetail

--- app/src/main/java/com/example/demo/Rdetail.java
+++ app/src/main/java/com/example/demo/Rdetail.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,12 +13,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Rdetail extends AppCompatActivity {
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +42,47 @@ public class Rdetail extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         Bundle extras = getIntent().getExtras();
+        String restaurantID = extras.getString("rID");
         String rname = extras.getString("rname");
         String raddress = extras.getString("raddress");
         TextView mName = findViewById(R.id.Restaurant_Name);
         TextView mAddress = findViewById(R.id.Restaurant_Address);
         mName.setText(rname);
         mAddress.setText(raddress);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userID = user.getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("sellers").child(restaurantID).child("Dishes");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            List<ItemData> itemDataList = new ArrayList<>();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    ItemData itemData = new ItemData();
+                    Dish dish = dataSnapshot1.getValue(Dish.class);
+                    itemData.ItemName = dish.DishName;
+                    itemData.Price = dish.DishPrice;
+                    itemDataList.add(itemData);
+                }
+
+                RecyclerView ItemList = findViewById(R.id.ItemRecyclerView);
+                ItemList.setHasFixedSize(true);
+                ItemList.setClickable(true);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                ItemList.setLayoutManager(linearLayoutManager);
+                ItemListAdapter itemListAdapter = new ItemListAdapter(itemDataList);
+                ItemList.setAdapter(itemListAdapter);
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         View one_item = (View) findViewById(R.id.ItemRecyclerView);
         one_item.setOnClickListener(new View.OnClickListener() {
