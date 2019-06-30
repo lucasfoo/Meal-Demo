10ade4add1139afebbb69fdc516c3917da654281
lucasfoo <lucasfoo33@gmail.com>
2019-06-30 20:08:13 +0800

    minor UI edit for navbar( gallery -> order)

--- app/src/main/java/com/example/demo/MainMenu.java
+++ app/src/main/java/com/example/demo/MainMenu.java
@@ -164,7 +164,7 @@ public class MainMenu extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_orders) {
 
         } else if (id == R.id.nav_slideshow) {
 
