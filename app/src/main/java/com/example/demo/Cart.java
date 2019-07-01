09e6ee2cd809e2877a595684742e9cf26b01fa5e
shaopeng <sleepingpandaaa@gmail.com>
2019-07-01 09:58:45 +0800

    cart on click

--- app/src/main/java/com/example/demo/Cart.java
+++ app/src/main/java/com/example/demo/Cart.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -99,6 +100,14 @@ public class Cart extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Cart.this, Checkout.class);
                 startActivity(intent);
+            }
+        });
+
+        ImageButton delete = (ImageButton) findViewById(R.id.delete_from_cart);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
