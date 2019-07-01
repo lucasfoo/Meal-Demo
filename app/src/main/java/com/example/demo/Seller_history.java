a7d1631a93c0f359c83dbab8cc7103e6aa0571bb
shaopeng <sleepingpandaaa@gmail.com>
2019-07-01 11:28:21 +0800

    add in tool bar for seller_history

--- app/src/main/java/com/example/demo/Seller_history.java
+++ app/src/main/java/com/example/demo/Seller_history.java
@@ -1,24 +1,39 @@
 package com.example.demo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Seller_history extends AppCompatActivity {
+public class Seller_history extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_history);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.seller_history_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.seller_history_nav);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
 
         RecyclerView recList = findViewById(R.id.seller_history_view);
         recList.setHasFixedSize(true);
@@ -43,5 +58,10 @@ public class Seller_history extends AppCompatActivity {
             items.add(data);
         }
         return  items;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
